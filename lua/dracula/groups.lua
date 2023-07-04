abaf53c60a7f02d16352da902191c1af6dd951ac
Emille Henry <emillenani@gmail.com>
2023-07-04 18:58:55 -0400

    Update groups.lua

--- lua/dracula/groups.lua
+++ lua/dracula/groups.lua
@@ -314,7 +314,7 @@ local function setup(configs)
       TelescopeNormal = { fg = colors.fg, bg = colors.bg, },
       TelescopeMatching = { fg = colors.green, },
       TelescopePromptPrefix = { fg = colors.purple, },
-      TelescopePromptTitle = { fg = colors.yellow ),
+      TelescopePromptTitle = { fg = colors.yellow },
       -- TelescopePromptPrefix
 
       -- NvimTree
