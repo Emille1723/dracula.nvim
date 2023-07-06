d28e1f5767344e4292cfdea78d23876fba4bd96d
Emille Henry <emillenani@gmail.com>
2023-07-06 19:39:42 -0400

    Update groups.lua

--- lua/dracula/groups.lua
+++ lua/dracula/groups.lua
@@ -313,7 +313,7 @@ local function setup(configs)
       -- TelescopeNormal = { fg = colors.fg, bg = colors.bg, },
       -- TelescopeNormal = { fg = colors.fg, bg = colors.charcoal, },
       TelescopeNormal = { fg = colors.fg, bg = colors.bg, },
-      TelescopeMatching = { fg = colors.orange, },
+      TelescopeMatching = { fg = colors.pink, },
       TelescopePromptTitle = { fg = colors.pink },
       TelescopePreviewTitle = { fg = colors.pink },
       -- TelescopeSelectionCaret = { fg = colors.green },
