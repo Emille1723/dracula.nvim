835563fa4bfac34bc6f403c83914e9228ba2106a
Emille Henry <emillenani@gmail.com>
2023-08-17 09:44:08 -0400

    Update groups.lua

--- lua/dracula/groups.lua
+++ lua/dracula/groups.lua
@@ -157,7 +157,7 @@ local function setup(configs)
       ['@boolean'] = { fg = colors.purple, },
       ['@float'] = { fg = colors.green, },
       ['@annotation'] = { fg = colors.yellow, },
-      ['@attribute'] = { fg = colors.pink, },
+      ['@attribute'] = { fg = colors.cyan, },
       ['@namespace'] = { fg = colors.orange, },
 
       -- ['@function.builtin'] = { fg = colors.cyan, },
