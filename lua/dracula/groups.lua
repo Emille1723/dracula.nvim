318f72eeac20cfa9cecf45df3f39d67eacace6f4
Emille Henry <emillenani@gmail.com>
2023-11-03 18:07:00 -0400

    Update groups.lua

--- lua/dracula/groups.lua
+++ lua/dracula/groups.lua
@@ -336,7 +336,7 @@ local function setup(configs)
       -- TelescopeSelectionCaret = { fg = "#FFCB8B", }, -- nightfly briight_yellow -- my default
       -- TelescopeSelectionCaret = { fg = "#191C24", bg = "#FFCB8B" }, -- nightfly bright_yellow -- my default with mods
       -- TelescopeSelectionCaret = { fg = "#191C24", bg = "#82AAFF" }, -- nightfly blue -- my default with more mods
-      TelescopeSelectionCaret = { fg = colors.yellow, bg = "#82AAFF" },
+      TelescopeSelectionCaret = { fg = colors.bright_yellow, bg = "#82AAFF" },
       TelescopePromptNormal = { fg = colors.green },
       TelescopePromptPrefix = { fg = colors.green, },
       TelescopeResultsDiffDelete = { fg = colors.red },
